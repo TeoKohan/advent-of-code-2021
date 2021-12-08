e1775d41213fff5bd3d46a7eaf86d1a1aedac41c
Firewill <Teo.Kohan@gmail.com>
2021-12-08 15:37:15 -0300

    Rewrite day 8 with list comprehension

--- Day 8/7-segment.py
+++ Day 8/7-segment.py
@@ -1,17 +1,17 @@
 I = [line.strip() for line in open('input')]
 
 I = [line.strip() for line in open('input')]
-O = list(map(lambda x: (x.split('|'))[1].strip(), I))
-O = list(map(lambda x: x.split(), O))
+O = [(x.split('|'))[1].strip() for x in I]
+O = [x.split() for x in O]
 O = [i for output in O for i in output]
-C = [1 for v in O if (len(v) in [2, 4, 3, 7]) ]
+C = [1 for v in O if (len(v) in [2, 3, 4, 7]) ]
 
 simple = sum(C)
 
-S = list(map(lambda x: (x.split('|'))[0].strip(), I))
-O = list(map(lambda x: (x.split('|'))[1].strip(), I))
-S = list(map(lambda x: x.split(), S))
-O = list(map(lambda x: x.split(), O))
+S = [(x.split('|'))[0].strip() for x in I]
+O = [(x.split('|'))[1].strip() for x in I]
+S = [x.split() for x in S]
+O = [x.split() for x in O]
 
 numbers = [
     set(['a', 'b', 'c', 'd', 'e', 'f', 'g']),
@@ -29,23 +29,23 @@ numbers = [
 complex = 0
 for i in range(len(S)):
     SS = S[i]
-    one   = list(filter(lambda x: len(x) == 2, SS))[0]
-    four  = list(filter(lambda x: len(x) == 4, SS))[0]
-    seven = list(filter(lambda x: len(x) == 3, SS))[0]
-    eight = list(filter(lambda x: len(x) == 7, SS))[0]
-    one_apparitions = [len(list(filter(lambda x: one[0] in x, SS))),
-                       len(list(filter(lambda x: one[1] in x, SS)))]
+    one   = [n for n in SS if len(n) == 2][0]
+    four  = [n for n in SS if len(n) == 4][0]
+    seven = [n for n in SS if len(n) == 3][0]
+    eight = [n for n in SS if len(n) == 7][0]
+    one_apparitions = [len([n for n in SS if one[0] in n]),
+                       len([n for n in SS if one[1] in n])]
     c = one[0] if one_apparitions[0] == 8 else one[1]
     f = one[0] if one_apparitions[0] == 9 else one[1]
-    a = list(filter(lambda x: x != c and x != f, seven))[0]
-    four = list(filter(lambda x: x != c and x != f, four))
-    four_apparitions = [len(list(filter(lambda x: four[0] in x, SS))),
-                        len(list(filter(lambda x: four[1] in x, SS)))]
+    a = [n for n in seven if n not in [c, f]][0]
+    four = [n for n in four if n not in [c, f]]
+    four_apparitions = [len([n for n in SS if four[0] in n]),
+                        len([n for n in SS if four[1] in n])]
     b = four[0] if four_apparitions[0] == 6 else four[1]
     d = four[0] if four_apparitions[0] == 7 else four[1]
-    eight = list(filter(lambda x: x != a and x != b and x != c and x != d and x != f, eight))
-    eight_apparitions = [len(list(filter(lambda x: eight[0] in x, SS))),
-                         len(list(filter(lambda x: eight[1] in x, SS)))]
+    eight = [n for n in eight if n not in [a, b, c, d, f]]
+    eight_apparitions = [len([n for n in SS if eight[0] in n]),
+                         len([n for n in SS if eight[1] in n])]
     e = eight[0] if eight_apparitions[0] == 4 else eight[1]
     g = eight[0] if eight_apparitions[0] == 7 else eight[1]
     #Decoding done
@@ -59,13 +59,14 @@ for i in range(len(S)):
         f: 'f',
         g: 'g'
     }
-    translation = list(map(lambda x: set(map(lambda y: dictionary[y], x)), OO))
+    translation = [[dictionary[y] for y in x] for x in OO]
+    translation = [set(x) for x in translation]
     password = [0] * 4
     for j in range(4):
         for k in range(10):
             if translation[j] == numbers[k]:
                 password[j] = k
-    password = int(''.join(list(map(str, password))))
+    password = int(''.join([str(x) for x in password]))
     complex += password
 
 output = open('output', 'w')
