b2bab8945d3312fbc72a3ab873fa1f905bf3c3cd
Firewill <Teo.Kohan@gmail.com>
2021-12-12 03:12:39 -0300

    Solve day 12

---
+++ Day 12/cave-system.py
@@ -0,0 +1,41 @@
+I = [line.strip() for line in open('input')]
+
+D = { }
+for line in I:
+    (S, E) = line.split('-')
+    if S in D:
+        D[S] += [E]
+    else:
+        D[S] = [E]
+    if E in D:
+        D[E] += [S]
+    else:
+        D[E] = [S]
+
+def move_cave(PS):
+    QS = []
+    for (PB) in PS:
+        P, B = PB
+        position = P[-1]
+        if position == 'end':
+            QS.append(PB)
+            continue
+        available = D[position]
+        available = [cave for cave in available if (cave.isupper()) or (cave not in P) or (cave not in ['start', 'end'] and B) ]
+        for cave in available:
+            QS.append( (P+[cave], B and (cave.isupper() or cave not in P)) )
+    return QS
+
+C = [(['start'], False)]
+while(next((P for (P, B) in C if P[-1] != 'end'), None) != None):
+    C = move_cave(C)
+simple_path = (len(C))
+
+C = [(['start'], True)]
+while(next((P for (P, B) in C if P[-1] != 'end'), None) != None):
+    C = move_cave(C)
+repeat_path = (len(C))
+
+output = open('output', 'w')
+output.write(str(simple_path)+'\n'+str(repeat_path)+'\n')
+output.close()
