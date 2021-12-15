f2d37294e1c6dccc1829921460c239f4e50092d0
Firewill <Teo.Kohan@gmail.com>
2021-12-15 15:56:44 -0300

    Refactor dijkstra to use dict instead of set

--- Day 15/dangerous-pathfinding.py
+++ Day 15/dangerous-pathfinding.py
@@ -3,30 +3,21 @@ from math import inf
 I = [line.strip() for line in open('input')]
 I = [[int(n) for n in line] for line in I]
 
-def dijkstra(grid):
+def dijkstra (grid):
     distances = [[inf for cell in row] for row in grid]
-
     D = [(0, 1), (1, 0), (0, -1), (-1, 0)]
  
-    S = set()
-    S.add((0, 0, 0))
-
+    S = { }
+    S[0, 0] = 0
     distances[0][0] = grid[0][0]
  
-    while (len(S) > 0):
-        x, y, d = min(S, key = lambda x: x[2])
-        S.remove((x, y, d))
+    while len(S) > 0:
+        x, y = min(S, key = lambda k: S[k])
+        del S[x, y]
  
-        for u, v in D:
-            if not (0 <= x+u < len(grid)) or not (0 <= y+v < len(grid)):
-                continue
-
-            if distances[x+u][y+v] > distances[x][y] + grid[x+u][y+v]:
-                if distances[x+u][y+v] != inf:
-                    S.remove((x, y, distances[x+u][y+v]))
-                distances[x+u][y+v] = distances[x][y] + grid[x+u][y+v]
-
-                S.add((x+u, y+v, distances[x+u][y+v]))
+        for u, v in [(x+u, y+v) for u, v in D if 0 <= x+u < len(grid[0]) and 0 <= y+v < len(grid)]:
+            if distances[u][v] > distances[x][y] + grid[u][v]:
+                S[u, v] = distances[u][v] = distances[x][y] + grid[u][v]
 
     return distances[-1][-1] - grid[0][0]
 
@@ -43,7 +34,6 @@ for i in range(1, 5):
     F = F + increase_matrix(R, i)
 
 big_cave = dijkstra(F)
-
 
 output = open('output', 'w')
 output.write(str(small_cave)+'\n'+ str(big_cave) +'\n')
